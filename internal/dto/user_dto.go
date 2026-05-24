7efffb04b0e6de3de8695462d231961ba3018ffe
obscure <konstantinkochengin44@gmail.com>
2026-05-24 11:45:18 +0700

    refactor: delete useless tags

--- internal/dto/user_dto.go
+++ internal/dto/user_dto.go
@@ -4,31 +4,31 @@ package dto
 import "time"
 
 type CreateUserDTO struct {
-	Name        string `json:"name" gorm:"not null;size:20"`
-	Username    string `json:"username" gorm:"not null;unique;size:16"`
-	Email       string `json:"email" gorm:"not null;unique;size:100"`
-	PhoneNumber string `json:"phone_number" gorm:"not null;unique;size:25"`
-	Age         int    `json:"age" gorm:"default:0"`
-	IsSmart     bool   `json:"is_smart" gorm:"default:false"`
+	Name        string `json:"name" binding:"required,max=20"`
+	Username    string `json:"username" binding:"required,max=16,min=4"`
+	Email       string `json:"email" binding:"required,email"`
+	PhoneNumber string `json:"phone_number" binding:"required,max=25"`
+	Age         int    `json:"age"`
+	IsSmart     bool   `json:"is_smart"`
 }
 
 type UpdateUserDTO struct {
-	Name        string `json:"name" gorm:"not null;size:20"`
-	Username    string `json:"username" gorm:"not null;unique;size:16"`
-	Email       string `json:"email" gorm:"not null;unique;size:100"`
-	PhoneNumber string `json:"phone_number" gorm:"not null;unique;size:25"`
-	Age         int    `json:"age" gorm:"default:0"`
-	IsSmart     bool   `json:"is_smart" gorm:"default:false"`
+	Name        string `json:"name" binding:"omitempty,max=20"`
+	Username    string `json:"username" binding:"omitempty,max=16,min=4"`
+	Email       string `json:"email" binding:"omitempty,email"`
+	PhoneNumber string `json:"phone_number" binding:"omitempty,max=25"`
+	Age         int    `json:"age"`
+	IsSmart     bool   `json:"is_smart"`
 }
 
 type UserDTO struct {
 	ID          int        `json:"id"`
-	Name        string     `json:"name" gorm:"not null;size:20"`
-	Username    string     `json:"username" gorm:"not null;unique;size:16"`
-	Email       string     `json:"email" gorm:"not null;unique;size:100"`
-	PhoneNumber string     `json:"phone_number" gorm:"not null;unique;size:25"`
-	Age         int        `json:"age" gorm:"default:0"`
-	IsSmart     bool       `json:"is_smart" gorm:"default:false"`
+	Name        string     `json:"name"`
+	Username    string     `json:"username"`
+	Email       string     `json:"email"`
+	PhoneNumber string     `json:"phone_number"`
+	Age         int        `json:"age"`
+	IsSmart     bool       `json:"is_smart"`
 	CreatedAt   time.Time  `json:"created_at"`
 	UpdatedAt   time.Time  `json:"updated_at"`
 	DeletedAt   *time.Time `json:"deleted_at"`
--- internal/models/user.go
+++ internal/models/user.go
@@ -4,14 +4,14 @@ package models
 import "time"
 
 type User struct {
-	ID          uint       `json:"id" gorm:"primaryKey;autoIncrement"`
-	Name        string `json:"name" gorm:"not null;size:20"`
-	Username    string `json:"username" gorm:"not null;unique;size:16"`
-	Email       string `json:"email" gorm:"not null;unique;size:100"`
-	PhoneNumber string `json:"phone_number" gorm:"not null;unique;size:25"`
-	Age         int    `json:"age" gorm:"default:0"`
-	IsSmart     bool   `json:"is_smart" gorm:"default:false"`
+	ID          uint       `json:"id" gorm:"primaryKey"`
+	Name        string     `json:"name"`
+	Username    string     `json:"username"`
+	Email       string     `json:"email"`
+	PhoneNumber string     `json:"phone_number"`
+	Age         int        `json:"age"`
+	IsSmart     bool       `json:"is_smart"`
 	CreatedAt   time.Time  `json:"created_at"`
-    UpdatedAt   time.Time  `json:"updated_at"`
-    DeletedAt   *time.Time `json:"deleted_at"`
+	UpdatedAt   time.Time  `json:"updated_at"`
+	DeletedAt   *time.Time `json:"deleted_at"`
 }
